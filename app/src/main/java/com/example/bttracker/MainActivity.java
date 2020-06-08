1546cac4c07cea278590dc4fc0a99fc1bd102b02
2020m020 <2020m020@kuas.ac.jp>
2020-06-08 18:27:36 +0900

    extracted string resources in all layout files and extracted string resources in all layout files and  implemented OnClickListener

--- app/src/main/java/com/example/bttracker/MainActivity.java
+++ app/src/main/java/com/example/bttracker/MainActivity.java
@@ -8,61 +8,83 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView mainButton1 = findViewById(R.id.MainButton1);
+        mainButton1.setOnClickListener(this);
+
+        TextView mainButton2 = findViewById(R.id.MainButton2);
+        mainButton2.setOnClickListener(this);
+
+        TextView mainButton3 = findViewById(R.id.MainButton3);
+        mainButton3.setOnClickListener(this);
+
+        TextView mainButton4 = findViewById(R.id.MainButton4);
+        mainButton4.setOnClickListener(this);
+
         createNotificationChannel();
     }
 
-    public void goToLogActivity(View view) {
-        Intent toLog = new Intent(this,LogActivity.class);
-        startActivity(toLog);
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.MainButton1:
+                Intent toLog = new Intent(this, LogActivity.class);
+                startActivity(toLog);
+                break;
+            case R.id.MainButton2:
+                Intent toNormal = new Intent(this, NormalActivity.class);
+                startActivity(toNormal);
+                break;
+            case R.id.MainButton3:
+                Intent toMechanism = new Intent(this, MechanismActivity.class);
+                startActivity(toMechanism);
+                break;
+            case R.id.MainButton4:
+                Intent openFeverLink = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.healthline.com/health/how-to-break-a-fever"));
+                if (openFeverLink.resolveActivity(getPackageManager()) != null) {
+                    startActivity(openFeverLink);
+                    break;
+                }
+        }
+
+
     }
 
-    public void goToNormalActivity(View view) {
-        Intent toNormal = new Intent(this,NormalActivity.class);
-        startActivity(toNormal);
-    }
-
-    public void goToMechanism(View view) {
-        Intent toMechanism = new Intent(this,MechanismActivity.class);
-        startActivity(toMechanism);
-    }
-
-    public void goToHandle(View view) {
-        Intent toHandle = new Intent(this,HandlingActivity.class);
-        startActivity(toHandle);
-    }
 
     public void SetReminder(View view) {
-        Toast.makeText(this,"Reminder set!", Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(this,ReminderBroadcastReceiver.class);
-        PendingIntent pd = PendingIntent.getBroadcast(this,0,intent,0);
-        AlarmManager alarmManager=(AlarmManager)getSystemService(ALARM_SERVICE);
-        long interval = 1000*6;
-        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,System.currentTimeMillis(),interval,pd);
+        Toast.makeText(this, "Reminder set!", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, ReminderBroadcastReceiver.class);
+        PendingIntent pd = PendingIntent.getBroadcast(this, 0, intent, 0);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        long interval = 1000 * 6;
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), interval, pd);
     }
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    private void createNotificationChannel(){
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            String channelID ="BT_Tracker_Channel";
-            String channelName="BTTrackerReminderChannel";
-            String channelIDescription="Channel for BT Tracker reminder";
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel= new NotificationChannel(channelID,channelName,importance);
-            channel.setDescription(channelIDescription);
-            NotificationManager notificationManager= getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
 
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String channelID = "BT_Tracker_Channel";
+            String channelName = "BTTrackerReminderChannel";
+            String channelIDescription = "Channel for BT Tracker reminder";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(channelID, channelName, importance);
+            channel.setDescription(channelIDescription);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
 
 
         }
